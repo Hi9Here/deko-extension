dc102b53e99578c983b289ac15600f9c7a97e61e
Marcus Hitchins <marcus7777@gmail.com>
2015-07-06 16:41:07 +0100

    starting

--- hi9.js
+++ hi9.js
@@ -1,3 +1,5 @@
+var tabList = {};
+var urlsToList = ["https://github.com/","chrome-devtools://devtools"];
 chrome.idle.onStateChanged.addListener(function(v) {
 
   console.log("it works! your ",v);
@@ -5,11 +7,17 @@ chrome.idle.onStateChanged.addListener(function(v) {
 });
 chrome.tabs.onActivated.addListener(function(v) {
 
-    console.log("Your url is", v);
+    console.log("Activated tab", v);
 
 })
 chrome.tabs.onUpdated.addListener(function(id,o,t) {
 
-    console.log("Your o.url", o.url);
+  chrome.tabs.getSelected(null,function(tab) {
+    console.log("Your url is",  tab.url);
+    //for 
+    //if () {
+
+    //}
+  });
 
 })
