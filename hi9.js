deb6852c79e8c6209bf34a2b8a545d176ac51672
Marcus Hitchins <marcus7777@gmail.com>
2015-07-06 16:22:36 +0100

    get tabs

--- hi9.js
+++ hi9.js
@@ -5,6 +5,13 @@ chrome.idle.onStateChanged.addListener(function(v) {
 });
 chrome.tabs.onActivated.addListener(function(v) {
 
-  console.log("it works! onActivated  ", v);
-
+  // Get the current active tab in the lastly focused window
+  chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+  }, function(tabs) {
+    // and use that tab to fill in out title and url
+    var tab = tabs[0];
+    console.log(tabs);
+  });
 })
