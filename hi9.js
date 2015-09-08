5375a7e3c39ec882c2af8ad48b5f26e7f5422f46
Marcus Hitchins <Marcus7777@gmail.com>
2015-09-08 10:42:50 +0100

    testing

--- hi9.js
+++ hi9.js
@@ -4,57 +4,57 @@ var idle = [];
 
 chrome.idle.onStateChanged.addListener(function(v) {
 
-  console.log("it works! your ",v);
-  console.log("idle ", idle);
-  var d = new Date();
-  var n = d.getTime();
-  
-  idle.push([n ,v])
-  chrome.storage.local.set({
-    'idle': idle
-  });
-  chrome.storage.local.get(['idle'], function(result) {
-    var idle_loaded = result.idle;
-    console.debug('idle loaded:', idle_loaded);
-  });
-  console.debug('idle :', idle);
+	console.log("it works! your ",v);
+	console.log("idle ", idle);
+	var d = new Date();
+	var n = d.getTime();
+
+	idle.push([n ,v])
+	chrome.storage.local.set({
+		'idle': idle
+	});
+	chrome.storage.local.get(['idle'], function(result) {
+		var idle_loaded = result.idle;
+		console.debug('idle loaded:', idle_loaded);
+	});
+	console.debug('idle :', idle);
 });
 chrome.tabs.onActivated.addListener(function(v) {
 
-    console.log("Activated tab", v);
-    chrome.storage.local.set({
-        'channels': [1, 2, 3],
-        'keywords': ['a', 'b', 'c']
-});
- 
-chrome.storage.local.get(['channels', 'keywords'], function(result) {
-        var channels = result.channels;
-        var keywords = result.keywords;
-        console.debug('channels :', channels);
-        console.debug('keywords :', keywords);
-});
+	console.log("Activated tab", v);
+	chrome.storage.local.set({
+		'channels': [1, 2, 3],
+		'keywords': ['a', 'b', 'c']
+	});
+
+	chrome.storage.local.get(['channels', 'keywords'], function(result) {
+		var channels = result.channels;
+		var keywords = result.keywords;
+		console.debug('channels :', channels);
+		console.debug('keywords :', keywords);
+	});
 
 })
 chrome.tabs.onUpdated.addListener(function(id,o,t) {
 
-  chrome.tabs.getSelected(null,function(tab) {
-    console.log("Your url is",  tab.url);
-    for (var i = 0; i < urlsToList.length; ++i) { 
-      if (tab.url.substring(0, urlsToList[i].length) == urlsToList[i]) {
-         console.log("logging this one")
-      }
-    }
-  });
-  chrome.storage.local.set({
-        'channels': [1, 2, 3],
-        'keywords': ['a', 'b', 'c']
-  });
- 
-chrome.storage.local.get(['channels', 'keywords'], function(result) {
-        var channels = result.channels;
-        var keywords = result.keywords;
-        console.debug('channels :', channels);
-        console.debug('keywords :', keywords);
+	chrome.tabs.getSelected(null,function(tab) {
+		console.log("Your url is",  tab.url);
+		for (var i = 0; i < urlsToList.length; ++i) { 
+			if (tab.url.substring(0, urlsToList[i].length) == urlsToList[i]) {
+				console.log("logging this one")
+			}
+		}
+	});
+	chrome.storage.local.set({
+		'channels': [1, 2, 3],
+		'keywords': ['a', 'b', 'c']
+	});
+
+	chrome.storage.local.get(['channels', 'keywords'], function(result) {
+		var channels = result.channels;
+		var keywords = result.keywords;
+		console.debug('channels :', channels);
+			console.debug('keywords :', keywords);
+	});
 });
 
-})
