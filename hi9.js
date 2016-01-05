98e072f922f65ebcb4ed74a2fa076397a4d2f5b8
MarcusHitchins <marcus@hhost.me>
2016-01-05 14:59:14 +0000

    starting saving

--- hi9.js
+++ hi9.js
@@ -1,82 +1,92 @@
-var tabList = {};
-var urlsToList = ["https://hi9.uk/","https://github.com/","chrome-devtools://devtools"];
-var idle = [];
-      var myFirebaseRef = new Firebase("https://hi9site.firebaseio.com/testing");
-      var authData = myFirebaseRef.getAuth();
-      function authHandler(error, authData) {
-        if (error) {
-          console.log("Login Failed!", error);
-        } else {
-          console.log("Authenticated successfully with payload:", authData);
-        }
-      }
-      if (authData) {
-        console.log("User " + authData.uid + " is logged in with " +  authData.provider);
-        var data = {}
-        data[authData.uid] = authData.google.email;
-        myFirebaseRef.set(data);
-      } else {
-        console.log("User is logged out");
-       
-        myFirebaseRef.authWithOAuthPopup("google", authHandler, {
-          remember: "sessionOnly",
-          scope: "email"
-        });
-      }
+var urlsToList = ['https://hi9.uk/', 'https://github.com/', 'chrome-devtools://devtools']
+var idle = []
+var userInfo
+var myFirebaseRef = new Firebase('https://hi9site.firebaseio.com/testing')
+var authData = myFirebaseRef.getAuth()
+var db = new PouchDB('hi9')
 
-chrome.idle.onStateChanged.addListener(function(v) {
+function authHandler (error, authData) {
+  if (error) {
+    console.log('Login Failed!', error)
+  } else {
+    console.log('Authenticated successfully with payload:', authData)
+  }
+}
+if (authData) {
+  console.log('User ' + authData.uid + ' is logged in with ' + authData.provider)
+  var data = {}
+  data[authData.uid] = authData.google.email
+  myFirebaseRef.set(data)
+} else {
+  console.log('User is logged out')
 
-	console.log("it works! your ",v);
-	console.log("idle ", idle);
-	var d = new Date();
-	var n = d.getTime();
+  myFirebaseRef.authWithOAuthPopup('google', authHandler, {
+    remember: 'sessionOnly',
+    scope: 'email'
+  })
+}
 
-	idle.push([n ,v])
-	chrome.storage.local.set({
-		'idle': idle
-	});
-	chrome.storage.local.get(['idle'], function(result) {
-		var idle_loaded = result.idle;
-		console.debug('idle loaded:', idle_loaded);
-	});
-	console.debug('idle :', idle);
-});
-chrome.tabs.onActivated.addListener(function(v) {
+chrome.idle.onStateChanged.addListener(function (v) {
+  console.log('it works! your ', v)
+  console.log('idle ', idle)
+  var d = new Date()
+  var n = d.getTime()
 
-	console.log("Activated tab", v);
-	chrome.storage.local.set({
-		'channels': [1, 2, 3],
-		'keywords': ['a', 'b', 'c']
-	});
-
-	chrome.storage.local.get(['channels', 'keywords'], function(result) {
-		var channels = result.channels;
-		var keywords = result.keywords;
-		console.debug('channels :', channels);
-		console.debug('keywords :', keywords);
-	});
-
+  idle.push([n, v])
+  chrome.storage.local.set({
+    'idle': idle
+  })
+  chrome.storage.local.get(['idle'], function (result) {
+    var idle_loaded = result.idle
+    console.debug('idle loaded:', idle_loaded)
+  })
+  console.debug('idle :', idle)
 })
-chrome.tabs.onUpdated.addListener(function(id,o,t) {
+chrome.tabs.onActivated.addListener(function (v) {
+  console.log('Activated tab', v)
+  chrome.storage.local.set({
+    'channels': [1, 2, 3],
+    'keywords': ['a', 'b', 'c']
+  })
 
-	chrome.tabs.getSelected(null,function(tab) {
-		console.log("Your url is",  tab.url);
-		for (var i = 0; i < urlsToList.length; ++i) { 
-			if (tab.url.substring(0, urlsToList[i].length) == urlsToList[i]) {
-				console.log("logging this one")
-			}
-		}
-	});
-	chrome.storage.local.set({
-		'channels': [1, 2, 3],
-		'keywords': ['a', 'b', 'c']
-	});
+  chrome.storage.local.get(['channels', 'keywords'], function (result) {
+    var channels = result.channels
+    var keywords = result.keywords
+    console.debug('channels :', channels)
+    console.debug('keywords :', keywords)
+  })
+})
+chrome.tabs.onUpdated.addListener(function (id, o, t) {
+  chrome.tabs.getSelected(null, function (tab) {
+    console.log('Your url is', tab.url)
+    for (var i = 0; i < urlsToList.length; ++i) {
+      if (tab.url.substring(0, urlsToList[i].length) === urlsToList[i]) {
+        console.log('logging this one', urlsToList[i], userInfo)
+      }
+    }
+  })
 
-	chrome.storage.local.get(['channels', 'keywords'], function(result) {
-		var channels = result.channels;
-		var keywords = result.keywords;
-		console.debug('channels :', channels);
-			console.debug('keywords :', keywords);
-	});
-});
+  chrome.storage.local.set({
+    'channels': [1, 2, 3],
+    'keywords': ['a', 'b', 'c']
+  })
 
+  chrome.storage.local.get(['channels', 'keywords'], function (result) {
+    var channels = result.channels
+    var keywords = result.keywords
+    console.debug('channels :', channels)
+    console.debug('keywords :', keywords)
+  })
+})
+
+chrome.identity.getProfileUserInfo(function (user) {
+  userInfo = user
+})
+
+db.put({
+  user: userInfo, _id: 'user'
+}).then(function (response) {
+  // handle response
+}).catch(function (err) {
+  console.log(err)
+})
